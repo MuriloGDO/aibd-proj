d285d840ca9a2e2db6af2a63b68953d298cee512
Murilo Gomes <gdo.murilo@gmail.com>
2024-08-28 17:19:31 -0300

    fix: STATICFILES_DIRS

--- aibd/aibd/settings.py
+++ aibd/aibd/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [    
-    os.path.join(BASE_DIR, 'templates/static'),
+    os.path.join(BASE_DIR, 'templates\static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
