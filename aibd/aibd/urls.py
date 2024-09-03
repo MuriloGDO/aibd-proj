ffafa9566c4a378e294589aa14f7c0f65576fa01
vitorColombiano <vicolombiano69@gmail.com>
2024-09-03 00:54:25 -0300

    implement consults

--- aibd/aibd/urls.py
+++ aibd/aibd/urls.py
@@ -5,6 +5,6 @@ from web import views as view_web
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view_web.home, name="home"),
-    path('unidade/<int:unit_id>/', view_web.unit_detail, name='unit_detail'),
-    path('inscrever/<int:unit_id>/', view_web.subscribe_to_the_unit, name='subscribe_to_the_unit'),
+    path('unidade/<str:unit_id>/', view_web.unit_detail, name='unit_detail'),
+    path('inscrever/<str:unit_id>/', view_web.subscribe_to_the_unit, name='subscribe_to_the_unit'),
 ]
--- aibd/web/views.py
+++ aibd/web/views.py
@@ -23,25 +23,26 @@ def home(request):
     return render(request, 'web_home.html', {'units': units})
 
 def unit_detail(request, unit_id):
-    #TODO: Recuperar os dados do cursinho pelo Id
-    ###Por enquanto vamos so simular um banco de dados
-    unit_alfa = {'Name': 'Unidade Alfa', 'Description': 'Descrição da unidade Alfa', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 3, 'Id': '1'}
-    unit_beta = {'Name': 'Unidade Beta', 'Description': 'Descrição da unidade Beta', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 5, 'Id': '2'}
-    unit_gama = {'Name': 'Unidade Gama', 'Description': 'Descrição da unidade Gama', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 6, 'Id': '3'}
-    unit_epsilon = {'Name': 'Unidade Épsilon', 'Description': 'Descrição da unidade Épsilon', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 2, 'Id': '4'}
-    unit_delta = {'Name': 'Unidade Delta', 'Description': 'Descrição da unidade Delta', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 1, 'Id': '5'}
-    unit_zeta = {'Name': 'Unidade Zeta', 'Description': 'Descrição da unidade Zeta', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 0, 'Id': '6'}
-    units = []
-    units.append(unit_alfa)
-    units.append(unit_beta)
-    units.append(unit_gama)
-    units.append(unit_epsilon)
-    units.append(unit_delta)
-    units.append(unit_zeta)
+    units = web.database_es.get_units()
+    # #TODO: Recuperar os dados do cursinho pelo Id
+    # ###Por enquanto vamos so simular um banco de dados
+    # unit_alfa = {'Name': 'Unidade Alfa', 'Description': 'Descrição da unidade Alfa', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 3, 'Id': '1'}
+    # unit_beta = {'Name': 'Unidade Beta', 'Description': 'Descrição da unidade Beta', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 5, 'Id': '2'}
+    # unit_gama = {'Name': 'Unidade Gama', 'Description': 'Descrição da unidade Gama', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 6, 'Id': '3'}
+    # unit_epsilon = {'Name': 'Unidade Épsilon', 'Description': 'Descrição da unidade Épsilon', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 2, 'Id': '4'}
+    # unit_delta = {'Name': 'Unidade Delta', 'Description': 'Descrição da unidade Delta', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 1, 'Id': '5'}
+    # unit_zeta = {'Name': 'Unidade Zeta', 'Description': 'Descrição da unidade Zeta', 'Fone': '(XX) XXXXX-XXXX', 'Address': 'Rua Exemplo, 123 - Cidade, Estado', 'NumberOfVailableVacancy': 0, 'Id': '6'}
+    # units = []
+    # units.append(unit_alfa)
+    # units.append(unit_beta)
+    # units.append(unit_gama)
+    # units.append(unit_epsilon)
+    # units.append(unit_delta)
+    # units.append(unit_zeta)
     ###
     ###Por enquanto simulando uma busca no banco de dados
     for unit in units:
-        if unit['Id'] == str(unit_id):
+        if unit['id'] == str(unit_id):
             return render(request, 'unit_detail.html', {'unit': unit})
     ###
 
@@ -64,6 +65,6 @@ def subscribe_to_the_unit(request, unit_id):
     ###
     ###Por enquanto simulando uma busca no banco de dados
     for unit in units:
-        if unit['Id'] == str(unit_id):
+        if unit['id'] == str(unit_id):
             return render(request, 'subscribe_to_the_unit.html', {'unit': unit})
     ###
